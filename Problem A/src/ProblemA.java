cffdb4fdaa0ff7f9591c77abcb3496ec4ee7069b
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-09-01 13:09:12 +0200

    Basic setup with main and helper function

---
+++ Problem A/src/ProblemA.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ProblemA {
+
+    private static String translate(String input){
+
+
+
+        return input;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println(translate(input));
+    }
+}
