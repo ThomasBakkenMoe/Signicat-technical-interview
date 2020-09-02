c807c2cf23cce12fbe825fbd858756df68464d61
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-09-02 17:23:25 +0200

    Added scanner and functionality for determining number of kittens and number of beds.

--- Problem C/src/ProblemC.java
+++ Problem C/src/ProblemC.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class ProblemC {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numbOfKittens = scanner.nextInt();
+        int numbOfBeds = scanner.nextInt();
+
+        System.out.println("Kittens: " + numbOfKittens);
+        System.out.println("Beds: " + numbOfBeds);
+
+        while (scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+
 
     }
 }
