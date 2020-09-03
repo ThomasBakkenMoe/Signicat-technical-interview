fbe4628d98849bde7bb4be8ef3df92e58828dbab
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-09-03 10:09:08 +0200

    Possible first functional version. Added Request private helper class. Added loops for booking at the kitten hotel.

--- Problem C/src/ProblemC.java
+++ Problem C/src/ProblemC.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProblemC {
@@ -8,14 +9,86 @@ public class ProblemC {
 
         int numbOfKittens = scanner.nextInt();
         int numbOfBeds = scanner.nextInt();
+        int maxKittenOccupancy = 0;
 
         System.out.println("Kittens: " + numbOfKittens);
         System.out.println("Beds: " + numbOfBeds);
 
+        ArrayList<Request> requests = new ArrayList<>();
+
+        int tempFrom;
+        int tempTo;
+
         while (scanner.hasNext()){
-            System.out.println(scanner.nextLine());
+            tempFrom = scanner.nextInt();
+            tempTo = scanner.nextInt();
+            requests.add(new Request(tempFrom, tempTo));
         }
 
+        int lastStayDay = 0;
 
+        for (Request request: requests) {
+
+            if (request.to > lastStayDay){
+                lastStayDay = request.to;
+            }
+        }
+
+        int[] occupancy = new int[lastStayDay + 1];
+
+        boolean bookingAvailable = true;
+        for (Request request: requests) {
+
+            for (int i = request.from; i <= request.to; i++) {
+                if (occupancy[i] >= numbOfBeds){
+                    bookingAvailable = false;
+                    break;
+                }
+
+            }
+            if (bookingAvailable){
+                for (int i = request.from; i <= request.to; i++) {
+                    occupancy[i] += 1;
+                }
+
+                maxKittenOccupancy++;
+
+            }
+
+            bookingAvailable = true;
+
+        }
+
+        System.out.println(maxKittenOccupancy);
+    }
+
+    /**
+     * Class for storing incoming requests
+     */
+    private static class Request{
+
+        private int from;
+        private int to;
+
+        Request(int from, int to){
+            this.from = from;
+            this.to = to;
+        }
+
+        public int getFrom() {
+            return from;
+        }
+
+        public void setFrom(int from) {
+            this.from = from;
+        }
+
+        public int getTo() {
+            return to;
+        }
+
+        public void setTo(int to) {
+            this.to = to;
+        }
     }
 }
